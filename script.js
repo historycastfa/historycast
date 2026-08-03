73f05e4dca89a68faeb1197a74d37cee64b2a659
historycastfa <pouriakohestani858@gmail.com>
2026-08-03 09:20:39 +0330

    Log latest object for debugging
    Log latest object to console before setting player source.

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ async function loadEpisodes() {
 
             document.querySelector(".episode p").innerHTML =
                 latest.description.substring(0, 300) + "...";
-            document.querySelector("#player").src = latest.enclosure.url;
+            console.log(latest);
 
     } catch (error) {
         console.log("RSS Error:", error);
